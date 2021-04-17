c7b18d9a80d07095e73f1e8b0e28a4cb90779b76
sega11456 <sega114567@gmail.com>
2021-04-17 16:57:56 +0700

    MainTeacher_done

--- src/components/Mainteacher/main-teacher.js
+++ src/components/Mainteacher/main-teacher.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import './main-teacher.css'
+import { FaChevronDown } from 'react-icons/fa';
 
 function Mteacher() {
+    
     return (
         <div className="top-text">
-            <h2>คำร้องขอที่รอพิจารณา</h2>
-                <button className="btn">วันที่ล่าสุด</button>
-                <button className="btn">ประเภทคำร้อง</button>
-                <button className="btn">เรียงตามชื่อ</button>
-                <button className="btn">เรียงตามรหัส</button>
+            <div className="top-x"><h2>คำร้องขอที่รอพิจารณา</h2></div>
+            
+
+            <div className="top-b">
+                <button className="btn">วันที่ล่าสุด  <FaChevronDown /></button>
+                <button className="btn">ประเภทคำร้อง  <FaChevronDown /></button>
+                <button className="btn">เรียงตามชื่อ  <FaChevronDown /></button>
+                <button className="btn">เรียงตามรหัส  <FaChevronDown /></button>
+            </div>    
 
                 <div className="top-box">
-                    <box className="bx">02/02/64 Ro-01 คำร้องทั่วไป มาลี ศรีสุข 60090500446</box>
-                    <button className="btn-detail">ดูรายละเอียด</button>
+                    <div className="bx">02/02/64 Ro-01 คำร้องทั่วไป มาลี ศรีสุข 60090500446
+                    <button className="btn-detail" onClick={() => {Mteacher()}}>ดูรายละเอียด</button>
+                    </div>
+                    
+                </div>
+
+                <div className="top-box">
+                    <div className="bx">02/02/64 Ro-01 คำร้องทั่วไป มาลี ศรีสุข 60090500446
+                    <button className="btn-detail" onClick={() => {Mteacher()}}>ดูรายละเอียด</button>
+                    </div>
+                    
                 </div>
         </div>
+        
     )
 };
+
 
 export default Mteacher;
