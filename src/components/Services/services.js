8e766e5bf8af9ce9b12b5cae020528c3dd94d7a9
sega11456 <sega114567@gmail.com>
2021-04-26 21:27:54 +0700

    add link 3 from

--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import './services.css'
+import { Link } from 'react-router-dom';
 
 
 const services = () => {
@@ -9,13 +10,21 @@ const services = () => {
             <h1 className="texthead">แบบฟอร์มเอกสาร</h1>
             <Row className="r-100">
                 <Col className="m-auto" sm={4}>
+                <Link className="line" to="/form1">
+
                     <button className="btn-form">คำร้องทั่วไป</button>
+                </Link>
                 </Col>
                 <Col className="m-auto" sm={4}>
-                    <button className="btn-form">คำขอลาป่วย /ลากิจ</button>
+                    <Link className="line" to="/form16">
+                        <button className="btn-form">คำขอลาป่วย /ลากิจ</button>
+                    </Link>
                 </Col>
                 <Col className="m-auto" sm={4}>
-                    <button className="btn-form">ใบลงทะเบียน เพิ่ม-ลด-ถอน-เปลี่ยนกลุ่ม-เปลี่ยนรายวิชา</button>
+                    <Link className="line" to="/form26">
+                        <button className="btn-form">ใบลงทะเบียน เพิ่ม-ลด-ถอน-เปลี่ยนกลุ่ม-เปลี่ยนรายวิชา</button>
+                    </Link>
+                    
                 </Col>
             </Row>
         </div>
