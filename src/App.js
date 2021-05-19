f56d07c76a3b78818bffbd5aee3514eb0521375a
famesensor <sathit.ju@outlook.com>
2021-05-19 20:50:38 +0700

    [:hammer:]: setup axios default and handler func login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react' ;
+import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route ,useLocation } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useLocation
+} from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 import Services from './pages/services';
@@ -8,40 +13,44 @@ import Signin from './pages/signin';
 import Form1 from './pages/f01';
 import Form16 from './pages/f16';
 import Form26 from './pages/f26';
-import { Helmet } from 'react-helmet'
+import { Helmet } from 'react-helmet';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+let axiosDefaults = require('axios/lib/defaults');
+axiosDefaults.baseURL = 'http://documentor-kmutt.me/api';
 
 export const Header = () => {
-  const location = useLocation();
-  if ( location.pathname !== '/signin'){
-    return <Navbar />
-  }else {
-    return ''
-  }
-}
+    const location = useLocation();
+    if (location.pathname !== '/signin') {
+        return <Navbar />;
+    } else {
+        return '';
+    }
+};
 
 function App() {
-  return (
-    <Router>
-      <Helmet><title>Documentor</title></Helmet>
-      <div className="app">
-        <Header />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' exact component={About} />
-          <Route path='/services' exact component={Services} />
-          <Route path='/signin' exact component={Signin} />
-          <Route path='/form1' exact component={Form1} />
-          <Route path='/form16' exact component={Form16} />
-          <Route path='/form26' exact component={Form26} />
-
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <Helmet>
+                <title>Documentor</title>
+            </Helmet>
+            <div className='app'>
+                <Header />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/about' exact component={About} />
+                    <Route path='/services' exact component={Services} />
+                    <Route path='/signin' exact component={Signin} />
+                    <Route path='/form1' exact component={Form1} />
+                    <Route path='/form16' exact component={Form16} />
+                    <Route path='/form26' exact component={Form26} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/signin/form-login.js
+++ src/components/signin/form-login.js
@@ -1,27 +1,59 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './form-login.css';
 
-function FormLogin (){
+function FormLogin() {
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
 
-    const [Username, setUsername] = useState('')
-    const [Password, setPassword] = useState('')
+    const LoginFunc = async () => {
+        let data = {
+            username: Username,
+            password: Password
+        };
 
-    const LoginFunc = () => {
-        console.log(Username , Password)
-    }
-    
+        try {
+            const res = await axios.post('/authentication/signin', data);
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <div className="form-login">
-            <h1 className="font_login">LOGIN</h1>
-            <div  className="form-input">
-                <label className="input-lb">Username</label>
-                <input className="text-input"  placeholder="username" value={Username} onChange={(e) => {setUsername(e.target.value)}}></input>
+        <div className='form-login'>
+            <h1 className='font_login'>LOGIN</h1>
+            <div className='form-input'>
+                <label className='input-lb'>Username</label>
+                <input
+                    className='text-input'
+                    placeholder='username'
+                    value={Username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                ></input>
             </div>
-            <div  className="form-input">
-                <label className="input-lb">Password</label>
-                <input className="text-input"  placeholder="password" type="password" value={Password} onChange={(e) => {setPassword(e.target.value)}}></input>
+            <div className='form-input'>
+                <label className='input-lb'>Password</label>
+                <input
+                    className='text-input'
+                    placeholder='password'
+                    type='password'
+                    value={Password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                ></input>
             </div>
-            <button className="btn-login" onClick={() => {LoginFunc()}} >LOGIN</button>
+            <button
+                className='btn-login'
+                onClick={() => {
+                    LoginFunc();
+                }}
+            >
+                LOGIN
+            </button>
         </div>
     );
 }
